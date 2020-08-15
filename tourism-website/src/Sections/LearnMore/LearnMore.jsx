a55f9bf0ed5d3e7069046a0bbbe82c0a7bc5c3a0
Matheus Werneck <werneckmatheus12@gmail.com>
2020-08-15 18:38:54 -0300

    Learn more section done

--- tourism-website/src/Sections/LearnMore/LearnMore.jsx
+++ tourism-website/src/Sections/LearnMore/LearnMore.jsx
@@ -4,25 +4,35 @@ import "./styles/LearnMore.css";
 //Core components
 import { Grid, Button } from "@material-ui/core";
 //Images
-import learnMoreImage from  '../../assets/images/learnMoreImage.png'
+import learnMoreImage from "../../assets/images/learnMoreImage.png";
 export default function LearnMore() {
   return (
-    <Grid spacing={6} justify="space-around" alignItems = "center" container direction="row">
-      <Grid direction="column" xl={5} lg={5}>
-        <img className="learnMoreIlustration"  src ={learnMoreImage} />
+    <Grid
+      lg={12}
+      xl={12}
+      spacing={6}
+      justify="space-around"
+      alignItems="center"
+      container
+      direction="row"
+    >
+      <Grid xl={1} />
+      <Grid direction="column" md={5} xs={10} xl={5} lg={5}>
+        <img className="learnMoreIlustration" src={learnMoreImage} />
       </Grid>
 
-      <Grid direction="column" xl={4} lg={5}>
+      <Grid direction="column" md={5} xs={12} xl={4} lg={5}>
         <h2 className="learnMoreTitle">A new way to explore the world</h2>
-        <p className= "learnMoreText">
-          For decades travellers have reached for Lonely Planet books when   
+        <p className="learnMoreText">
+          For decades travellers have reached for Lonely Planet books when
           looking to plan and execute their perfect trip, but now, they can also
           let Lonely Planet Experiences lead the way
         </p>
-        <Button    className="learnMoreButton" variant ="contained">
-          Learn More  
+        <Button className="learnMoreButton" variant="contained">
+          Learn More
         </Button>
       </Grid>
+      <Grid xl={1} />
     </Grid>
   );
 }
